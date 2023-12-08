63d267f1ac0fbb97717c73a5d701db41224a7ed5
Zero0242 <91895271+Zero0242@users.noreply.github.com>
2023-12-08 20:38:53 -0300

    Update GifExpertApp.jsx

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,11 +1,26 @@
+import { useState } from "react";
+
 export const GifExpertApp = () => {
+  const [categories, setCategories] = useState(["One Punch","Dragon Ball"]);
+
   return (
     <>
       {/* Titulo */}
       <h1>Gif Expert App</h1>
       {/* Input */}
       {/* Gif Grid */}
-        {/* Gif Item */}
+      {/* Gif Item */}
+      <ol>
+        <li>ABC</li>
+        <li>123</li>
+        <li>XYZ</li>
+      </ol>
+
+      <ol>
+        {categories.map((category, index) => (
+          <li key={category + index}>{category}</li>
+        ))}
+      </ol>
     </>
   );
 };
