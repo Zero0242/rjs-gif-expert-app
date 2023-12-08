5ea8e0da75090f3863cc555e70770d4e95ae683b
Zero0242 <91895271+Zero0242@users.noreply.github.com>
2023-12-08 20:31:15 -0300

    Añadido los styles

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,7 +1,11 @@
 export const GifExpertApp = () => {
   return (
     <>
+      {/* Titulo */}
       <h1>Gif Expert App</h1>
+      {/* Input */}
+      {/* Gif Grid */}
+        {/* Gif Item */}
     </>
   );
 };
