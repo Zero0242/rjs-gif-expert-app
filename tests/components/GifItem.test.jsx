3ac58642c86aa68297ca9f9f89ceb23686ecf0d5
Zero0242 <91895271+Zero0242@users.noreply.github.com>
2023-12-10 18:32:42 -0300

    Pruebas adicionales al GifItem

--- tests/components/GifItem.test.jsx
+++ tests/components/GifItem.test.jsx
@@ -2,12 +2,28 @@ import { GifItem } from "../../src/components";
 import { render, screen } from "@testing-library/react";
 
 describe("Pruebas en <GifItem />", () => {
+  const url = "https://tenor.com/hIiAum6gFkk.gif";
+  const title = "Pruebas";
+
   test("Deberia hacer match con el snapshot", () => {
-    const url = "https://tenor.com/hIiAum6gFkk.gif";
-    const title = "Pruebas";
     const { container } = render(<GifItem title={title} url={url} />);
 
     screen.debug();
     expect(container).toMatchSnapshot();
+  });
+
+  test("debe de mostrar la imagen con el URL y el ALT indicado", () => {
+    render(<GifItem title={title} url={url} />);
+
+    const { src, alt } = screen.getByRole("img");
+
+    expect(src).toBe(url);
+    expect(alt).toBe(title);
+  });
+
+  test("debe de mostrar el titulo en el componente", () => {
+    render(<GifItem title={title} url={url} />);
+
+    expect(screen.getByText(title)).toBeTruthy();
   });
 });
